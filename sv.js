31ea6298246f9bc6a1d746c5b43474280d2156e4
bootstrap41 <yunuskoskan@gmail.com>
2026-07-10 13:44:30 +0300

    refactor: logolar assets/logo klasörüne taşındı ve manifest güncellendi

---
+++ sv.js
@@ -0,0 +1,10 @@
+// PWA yükleme kriterini karşılamak için basit bir servis işçi yapısı
+self.addEventListener('install', (e) => {
+	  self.skipWaiting();
+	  });
+
+	  self.addEventListener('fetch', (e) => {
+	  	  // Sitenin stabil çalışması için istekleri normal şekilde iletir
+	  	  });
+	  })
+})
